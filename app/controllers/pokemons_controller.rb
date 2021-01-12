3b4091c997b47b3da1d6c4bb394c45b5fbbf40f2
Leamsi Escribano <leamsi.esc@gmail.com>
2021-01-12 12:21:23 -0500

    Final updates

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -10,7 +10,7 @@ class PokemonsController < ApplicationController
 
     def update
         @pokemon = Pokemon.find(params[:id])
-        @pokemon.update(pokemon_params)
+        @pokemon.update(pokemon_params)                                                                                   
         redirect_to root_path
     end
     
